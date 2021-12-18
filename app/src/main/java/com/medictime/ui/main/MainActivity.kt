57b7e044480295ab0a90d66dde2dbd7627735f35
Ubean <ubeannn@gmail.com>
2021-12-19 01:03:28 +0700

    Management Directory, Splash UI, App Icon, Dependencies

--- app/src/main/java/com/medictime/ui/main/MainActivity.kt
+++ app/src/main/java/com/medictime/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.medictime
+package com.medictime.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.medictime.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
