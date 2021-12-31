355500cde9bb7736b87ce65decd927e6f30bba03
Ubean <ubeannn@gmail.com>
2022-01-01 04:02:50 +0700

    Edit and Delete on EditMedicineActivity

--- app/src/main/java/com/medictime/ui/edit_medicine/EditMedicineActivity.kt
+++ app/src/main/java/com/medictime/ui/edit_medicine/EditMedicineActivity.kt
@@ -1,19 +1,32 @@
 package com.medictime.ui.edit_medicine
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.medictime.App
+import com.medictime.R
 import com.medictime.databinding.ActivityEditMedicineBinding
 import com.medictime.entity.Medicine
 import com.medictime.preferences.UserPreferences
 import java.text.SimpleDateFormat
-import java.time.OffsetDateTime
-import java.time.OffsetTime
+import java.time.*
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -42,6 +55,202 @@ class EditMedicineActivity : AppCompatActivity() {
         viewModel.getUserSetting().observe(this, { dataUser ->
             userId = if (dataUser.email.isNotEmpty()) viewModel.getUserIdByEmail(dataUser.email) else 0
         })
+
+        with(binding) {
+            medicine?.let { dataMedicine ->
+                with(cardMedicine) {
+                    Glide.with(image.context)
+                        .load(
+                            when (dataMedicine.type) {
+                                "Tablet" -> R.drawable.ic_medicine_tablet
+                                "Syrup" -> R.drawable.ic_medicine_syrup
+                                "Capsule" -> R.drawable.ic_medicine_capsule
+                                else -> R.drawable.logo
+                            }
+                        )
+                        .into(image)
+                    name.text = String.format("%dx %s", dataMedicine.amount, dataMedicine.name)
+                    description.text = dataMedicine.description
+                    time.text = dataMedicine.dateTime.format(timeFormat)
+                }
+
+                inputName.editText?.setText(dataMedicine.name)
+                inputType.editText?.setText(dataMedicine.type)
+                inputDescription.editText?.setText(dataMedicine.description)
+                inputDate.editText?.setText(dataMedicine.dateTime.format(DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy", Locale.getDefault())))
+                inputTime.editText?.setText(dataMedicine.dateTime.format(timeFormat))
+                inputAmount.editText?.setText(dataMedicine.amount.toString())
+            }
+
+            with(inputType) {
+                val items = listOf("Tablet", "Syrup", "Capsule")
+                val adapter = ArrayAdapter(binding.root.context, R.layout.list_item, items)
+                (editText as? AutoCompleteTextView)?.setAdapter(adapter)
+            }
+        }
+
+        binding.inputName.editText?.setOnFocusChangeListener { _, focus ->
+            binding.cardMedicine.name.text = when {
+                (binding.inputAmount.editText?.text?.isNotEmpty() == true) and !focus -> "${binding.inputAmount.editText?.text}x ${binding.inputName.editText?.text}"
+                (binding.inputAmount.editText?.text?.isEmpty() == true) and !focus -> binding.inputName.editText?.text
+                else -> binding.cardMedicine.name.text
+            }
+        }
+
+        binding.inputType.editText?.setOnFocusChangeListener { _, _ ->
+            Glide.with(binding.root.context)
+                .load(
+                    when (binding.inputType.editText?.text.toString()) {
+                        "Tablet" -> R.drawable.ic_medicine_tablet
+                        "Syrup" -> R.drawable.ic_medicine_syrup
+                        "Capsule" -> R.drawable.ic_medicine_capsule
+                        else -> R.drawable.logo
+                    }
+                )
+                .into(binding.cardMedicine.image)
+        }
+
+        binding.inputDescription.editText?.setOnFocusChangeListener { _, focus ->
+            binding.cardMedicine.description.text = when {
+                (binding.inputDescription.editText?.text?.isNotEmpty() == true) and !focus -> binding.inputDescription.editText?.text
+                (binding.inputDescription.editText?.text?.isEmpty() == true) and !focus -> "description"
+                else -> binding.cardMedicine.description.text
+            }
+        }
+
+        binding.inputAmount.editText?.setOnFocusChangeListener { _, focus ->
+            binding.cardMedicine.name.text = when {
+                (binding.inputName.editText?.text?.isNotEmpty() == true) and (binding.inputAmount.editText?.text?.isNotEmpty() == true) and !focus -> "${binding.inputAmount.editText?.text}x ${binding.inputName.editText?.text}"
+                (binding.inputName.editText?.text?.isEmpty() == true) and (binding.inputAmount.editText?.text?.isNotEmpty() == true) and !focus -> "${binding.inputAmount.editText?.text}x Medicine Name"
+                (binding.inputName.editText?.text?.isNotEmpty() == true) and (binding.inputAmount.editText?.text?.isEmpty() == true) and !focus -> binding.inputName.editText?.text
+                (binding.inputName.editText?.text?.isEmpty() == true) and (binding.inputAmount.editText?.text?.isEmpty() == true) and !focus -> "Medical Name"
+                else -> binding.cardMedicine.name.text
+            }
+        }
+
+        binding.datePicker.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val start = LocalDateTime
+                .of(
+                    calendar.get(Calendar.YEAR) - 0,
+                    calendar.get(Calendar.MONTH) + 1,
+                    calendar.get(Calendar.DAY_OF_MONTH) - 0,
+                    0,
+                    0,
+                    0,
+                )
+                .atZone(ZoneId.ofOffset("UTC", ZoneOffset.UTC))
+                .toInstant()
+                .toEpochMilli()
+            val constraintsBuilder = CalendarConstraints.Builder()
+                .setStart(start)
+                .setOpenAt(dayEpoch)
+                .setValidator(DateValidatorPointForward.from(start))
+            val picker = MaterialDatePicker.Builder.datePicker()
+                .setSelection(dayEpoch)
+                .setCalendarConstraints(constraintsBuilder.build())
+                .build()
+            picker.show(this.supportFragmentManager, DATE_PICKER_TAG)
+            picker.addOnPositiveButtonClickListener {
+                dayEpoch = it
+                binding.inputDate.editText?.setText(dateFormat.format(dayEpoch))
+            }
+        }
+
+        binding.timePicker.setOnClickListener {
+            val picker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(timeEpoch.hour)
+                .setMinute(timeEpoch.minute)
+                .build()
+            picker.show(this.supportFragmentManager, TIME_PICKER_TAG)
+            picker.addOnPositiveButtonClickListener {
+                timeEpoch = OffsetTime.of(picker.hour, picker.minute, 0, 0, OffsetTime.now().offset)
+                with(binding) {
+                    inputTime.editText?.setText(timeEpoch.format(timeFormat))
+                    cardMedicine.time.text = timeEpoch.format(timeFormat)
+                }
+            }
+        }
+
+        binding.btnSave.setOnClickListener {
+            closeKeyboard()
+
+            with(binding) {
+                val isNameFilled = isInputFilled(inputName, "Please fill name of medicine")
+                val isTypeFilled = isInputFilled(inputType, "Please select type of medicine")
+                val isDescriptionFilled = isInputFilled(inputDescription, "Please fill description of medicine")
+                val isDateFilled = isInputFilled(inputDate, "Please select date of reminder")
+                val isTimeFilled = isInputFilled(inputTime, "Please select time of reminder")
+                val isAmountFilled = isInputFilled(inputAmount, "Please fill amount of medicine")
+
+                if (isNameFilled and isTypeFilled and isDescriptionFilled and isDateFilled and isTimeFilled and isAmountFilled) {
+                    medicine?.let { dataMedicine ->
+                        viewModel.editMedicine(
+                            context = this@EditMedicineActivity,
+                            medicine = dataMedicine,
+                            user_id = userId,
+                            medicine_name = inputName.editText?.text.toString(),
+                            medicine_type = inputType.editText?.text.toString(),
+                            medicine_description = inputDescription.editText?.text.toString(),
+                            medicine_date = OffsetDateTime.ofInstant(Instant.ofEpochMilli(dayEpoch), ZoneId.systemDefault()),
+                            medicine_time = timeEpoch,
+                            medicine_amount = inputAmount.editText?.text.toString().toInt()
+                        )
+                    }
+                    onBackPressed()
+                }
+            }
+        }
+
+        binding.btnDelete.setOnClickListener {
+            closeKeyboard()
+            medicine?.let { dataMedicine ->
+                viewModel.deleteMedicine(this@EditMedicineActivity, dataMedicine)
+            }
+            onBackPressed()
+        }
+
+        binding.btnBack.setOnClickListener {
+            closeKeyboard()
+            onBackPressed()
+        }
+
+        viewModel.notificationText.observe(this, {
+            it.getContentIfNotHandled()?.let { text ->
+                showSnackBar(text)
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun isInputFilled(view: TextInputLayout, error: String) : Boolean {
+        view.editText?.clearFocus()
+        return if (TextUtils.isEmpty(view.editText?.text.toString().trim())) {
+            view.isErrorEnabled = true
+            view.error = error
+            false
+        } else {
+            view.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun showSnackBar(text: String) {
+        Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun closeKeyboard() {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 
     companion object {
--- app/src/main/java/com/medictime/ui/edit_medicine/EditMedicineViewModel.kt
+++ app/src/main/java/com/medictime/ui/edit_medicine/EditMedicineViewModel.kt
@@ -1,16 +1,20 @@
 package com.medictime.ui.edit_medicine
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.medictime.entity.Medicine
 import com.medictime.entity.User
 import com.medictime.helper.Event
 import com.medictime.helper.NotificationMedicine
 import com.medictime.preferences.UserPreferences
 import com.medictime.repository.MedicineRepository
 import com.medictime.repository.UserRepository
+import java.time.OffsetDateTime
+import java.time.OffsetTime
 
 class EditMedicineViewModel(application: Application, private val preferences: UserPreferences) : ViewModel() {
     private val mUserRepository: UserRepository = UserRepository(application)
@@ -22,4 +26,31 @@ class EditMedicineViewModel(application: Application, private val preferences: U
     fun getUserSetting(): LiveData<User> = preferences.getUserSetting().asLiveData()
 
     fun getUserIdByEmail(email: String): Int = mUserRepository.getUserByEmail(email).id
+
+    fun editMedicine(context: Context, user_id: Int, medicine: Medicine, medicine_name: String, medicine_type: String, medicine_description: String, medicine_date: OffsetDateTime, medicine_time: OffsetTime, medicine_amount: Int) {
+        with(medicine) {
+            userId = user_id
+            name = medicine_name
+            type = medicine_type
+            description = medicine_description
+            dateTime = medicine_time.atDate(medicine_date.toLocalDate())
+            amount = medicine_amount
+        }
+        mMedicineRepository.update(medicine)
+        notification.delete(context, medicine.id)
+        notification.set(
+            context,
+            medicine.id,
+            "${medicine.amount}x ${medicine.name} (${medicine.type})",
+            medicine.description,
+            medicine.dateTime
+        )
+        _notificationText.value = Event("Success update $medicine_name on database")
+    }
+
+    fun deleteMedicine(context: Context, medicine: Medicine) {
+        notification.delete(context, medicine.id)
+        mMedicineRepository.delete(medicine)
+        _notificationText.value = Event("Success delete ${medicine.name} on database")
+    }
 }
